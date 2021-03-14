e8be0f89ccc87bd5698c2088d70d94c521bab8b7
DevSonny <son8795@gmail.com>
2021-03-14 22:05:45 +0900

    PART2 Spring MVC Setting
    PART2 Spring MVC Setting (~147p)

---
+++ Spring/zerock/jex00/src/main/java/org/zerock/sample/Chef.java
@@ -0,0 +1,11 @@
+package org.zerock.sample;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component
+@Data // Lombok setter & 생성자, toString() 생성해줌
+public class Chef {
+	
+}
